121e5ee35da79d2ea1cd9ff42528ba9db54acc5b
merkuspb <msemenova21@gmail.com>
2023-01-27 10:27:16 +0300

    dependensis

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,16 @@
 module.exports = {
   env: {
+    browser: true,
     commonjs: true,
     es2021: true,
     node: true,
   },
   extends: 'airbnb-base',
-  overrides: [],
+  overrides: [
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
-    'no-console': 0,
-    'comma-dangle': 0,
-    'class-methods-use-this': 0,
-    'no-use-before-define': 0,
   },
 };
