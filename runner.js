d0b77baa9e6d62dfd54c5cfe95e35e1773145034
Alisa Savorskaya <alyssen95@mail.ru>
2023-01-27 13:24:00 +0300

    created runner.js and class Question

---
+++ runner.js
@@ -0,0 +1,20 @@
+const readlineSync = require('readline-sync');
+const Question = require('./Question');
+// const View = require('./View');
+// const Theme = require('./Theme');
+
+// вызов функции показа темы
+// const theme1 = View.showTheme();
+
+// вызов метода класса по цифре
+// const questionAndAnsw = Theme.getQandA(theme1)
+// получаем массив с вопросами и ответами по теме
+
+// создаем экз класса Вопрос!
+// const question1 = new Question(
+//   questionAndAnsw[0][0],
+//   questionAndAnsw[1][0],
+// );
+
+// показываем вопрос и получаем ответ от пользователя
+// const q1 = View.showQuestions(question1);
