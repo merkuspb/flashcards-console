159413c760086dc6f55c09c80c56b01e695e3ed5
Igor Poluektov <igorpoluektov74@gmail.com>
2023-01-27 13:14:41 +0300

    All in one Method

--- Theme.js
+++ Theme.js
@@ -8,43 +8,30 @@
 /* eslint-disable class-methods-use-this */
 const fs = require('fs');
 
-const userNum = 1;
+const userNum = 2;
 const userNumToInd = userNum - 1;
 
 class Theme {
-  static getAllThemes() {
+  static getQandA(userNumToInd) {
     const allThemes = fs.readdirSync('./topics');
-    return allThemes;
-  }
-
-  static getThemeStrings(userNumToInd, allThemes) {
     const pickedTheme = allThemes[userNumToInd];
     const questionStrings = fs.readFileSync(`./topics/${pickedTheme}`, 'utf-8');
-    return questionStrings;
-  }
-
-  static getQuestion(themeStrings) {
-    const stringsToArr = themeStrings.split('\r\n');
+    const stringsToArr = questionStrings.split('\r\n');
     const onlyQuestionsArr = [];
-    stringsToArr.map((str) => {
-      if (str.includes('?') && str.length >= 2) onlyQuestionsArr.push(str);
-    });
-    return onlyQuestionsArr;
-  }
-
-  static getAnswers(themeStrings) {
-    const questionArr = themeStrings.split('\r\n');
     const onlyAnswersArr = [];
-    questionArr.map((str) => {
-      if (!str.includes('?') && str.length >= 2) onlyAnswersArr.push(str);
+    stringsToArr.map((str) => {
+      if (str.includes('?') && str.length >= 2) {
+        onlyQuestionsArr.push(`${stringsToArr.indexOf(str) / 3 + 1}. ${str}`);
+      }
+      if (!str.includes('?') && str.length >= 2) {
+        onlyAnswersArr.push(str);
+      }
     });
-    return onlyAnswersArr;
+    const arrs = [onlyQuestionsArr, onlyAnswersArr];
+    return arrs;
   }
 }
 
-const allThemes = Theme.getAllThemes();
-const themeStrings = Theme.getThemeStrings(userNumToInd, allThemes);
-const questions = Theme.getQuestion(themeStrings);
-const answers = Theme.getAnswers(themeStrings);
+const questions = Theme.getQandA(userNumToInd);
 console.log(questions);
-console.log(answers);
+module.exports = Theme;
